52c9dcf8d9bc218e50a97d2fb176ce77cebdcf2c
AswinHaridasan <aswinharidasan6777@gmail.com>
2025-06-05 08:24:18 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import gsap from './tech/gsap.png'
+import framer from './tech/framer.png';
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import gearXpert from './p1.jpg';
+import project3 from './p3.jpg';
+import project2 from './p2.jpg'
+import mysql from './tech/mysql.png';
+import express from './tech/express.png';
+import aws from './tech/aws.png';
+import mui from './tech/mui.png'
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+
+import python from "./tech/python.png";
+import django from "./tech/django.png";
+import postgresql from "./tech/postgresql.png";
+import powerbi from "./tech/powerbi.png";
+import excel from "./tech/excel.png";
+import ps from "./tech/ps.png";
+
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import firstTestimonial from './Tars Ken.webp';
+import secondTestimonial from './image.webp';
+import thirdTestimonial from './third testimonial.webp'
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  gearXpert,
+  project2,
+  project3,
+  mysql,
+  express,
+  aws,
+  mui,
+  firstTestimonial,
+  secondTestimonial,
+  thirdTestimonial,
+  gsap,
+  framer,
+  figma,
+  git,
+  html,
+
+  python,
+  django,
+  postgresql,
+  powerbi,
+  excel,
+  ps,
+
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+
+};
---
+++ src/constants/index.js
@@ -0,0 +1,349 @@
+
+import {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  gearXpert,
+  project2,
+  project3,
+  mysql,
+  express,
+  aws,
+  mui,
+  
+  gsap,
+  framer,
+  figma,
+  git,
+  html,
+
+  python,
+  django,
+  postgresql,
+  powerbi,
+  excel,
+  ps,
+
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  firstTestimonial,
+  secondTestimonial,
+  thirdTestimonial,
+} from '../assets'
+
+
+// Import Tekisky separately
+import Wahy from "../assets/company/Wahy.png";
+import zignature from "../assets/company/zignature.png";
+import Selfie from "../assets/company/Selfie.png";
+
+
+export const navLinks = [
+
+
+  {
+    id: "about",
+    title: "About",
+    
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full-Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Wedding Photographer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "django",
+    icon: django,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "MySql",
+    icon: mysql,
+  },
+  {
+    name: "postgresql",
+    icon: postgresql,
+  },
+  {
+    name: "powerbi",
+    icon: powerbi,
+  },
+  {
+    name: "excel",
+    icon: excel,
+  },
+  {
+    name: "ps",
+    icon: ps,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+
+
+
+
+
+  // {
+  //   name: "React JS",
+  //   icon: reactjs,
+  // },
+  // {
+  //   name: "gsap",
+  //   icon: gsap,
+  // },
+  // {
+  //   name: "framer",
+  //   icon: framer,
+  // },
+
+ 
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
+  // {
+  //   name: "Tailwind CSS",
+  //   icon: tailwind,
+  // },
+  // {
+  //   name: "Material Ui",
+  //   icon: mui,
+  // },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  // {
+  //   name: "Express Js",
+  //   icon: express,
+  // },
+  {
+    name: "AWS",
+    icon: aws,
+  },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  
+
+ 
+
+];
+
+const experiences = [
+  {
+    title: "Python Full-Stack Developer",
+    company_name: "Wahy Labs Solutions",
+    icon: Wahy,
+    iconBg: "#383E56",
+    date: "May 2024 - May 2025",
+    points: [
+      "Studied and gained expertise in advanced Python and the Django framework for web development.",
+      "Developed and designed dynamic web applications using HTML, CSS, and JavaScript.",
+      "Gained hands-on experience with full-stack development, including both frontend and backend technologies..",
+      "Collaborated with mentors and team members to enhance problemsolving and coding efficiency.",
+    ],
+  },
+  {
+    title: "Education Consultant",
+    company_name: "Zignature",
+    icon: zignature, 
+    iconBg: "#383E56",
+    date: "2022 - PRESENT",
+    points: [
+      "Guiding students through academic and career pathways at Zignature Education Consultancy.",
+      "Providing expert advice on educational programs and assisting with admissions processes.",
+      "Passionate about helping individuals achieve their educational goals with personalized support.",
+    
+    
+    ],
+  },
+  {
+    title: "Wedding Photographer",
+    company_name: "Selfie Wedding Studio",
+    icon: Selfie, 
+    iconBg: "#383E56", 
+    date: "2018 - PRESENT",
+    points: [
+      "Passionate wedding photographer with a keen eye for capturing timeless moments.",
+      "Started my photography journey in 2018 and honed skills in storytelling, composition, and client collaboration.",
+      "Committed to delivering high-quality, memorable images that reflect the essence of every special occasion.",
+    ],
+  },
+];
+
+
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Huzaif proved me wrong.",
+    name: "MD Mustaqeem",
+    designation: "Ecommerce",
+    company: "QuickMart",
+    image: firstTestimonial,
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Huzaif does.",
+    name: "Abdul Raheman",
+    designation: "Ecommerce Business",
+    company: "justbuyz",
+    image: secondTestimonial,
+  },
+  {
+    testimonial:
+      "After Huzaif optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "James Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: thirdTestimonial,
+  },
+];
+
+const projects = [
+  {
+    name: "E-Learning platform",
+    description:
+      "An E-Learning platform is an online system for delivering education. It allows students to access courses, video lessons, and quizzes, while instructors manage content and track progress. It supports flexible, self-paced learning on any device.",
+    tags: [
+      {
+        name: "Python-Django",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML-CSS",
+        color: "white-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+ 
+    ],
+    image: project2,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Guruvayor Devaswom online-booking",
+    description:
+      "A digital platform for booking temple services like darshan, poojas, marriage functions, and accessing birth star (nakshatra) details. It offers easy and convenient access for devotees.",
+
+    tags: [
+      {
+        name: "Python-Django",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML-CSS",
+        color: "white-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+ 
+    ],
+    image: gearXpert,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "FarmXpert",
+    description:
+      "The E-Farms Seed Description System helps manage planting materials efficiently. It replaces manual notebooks with a digital platform and improving accuracy. This system automates seed tracking, purchases, and customer payments.",
+    tags: [
+      {
+        name: "Python-Django",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML-CSS",
+        color: "white-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+ 
+    ],
+    image: project3,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
